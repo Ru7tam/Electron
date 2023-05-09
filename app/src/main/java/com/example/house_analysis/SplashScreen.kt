b4b4bc39d4a9870372ccec7913356106c1b8055c
Ru7tam <105158227+Ru7tam@users.noreply.github.com>
2023-05-09 18:22:15 +0300

    Initial commit

---
+++ app/src/main/java/com/example/house_analysis/SplashScreen.kt
@@ -0,0 +1,22 @@
+package com.example.house_analysis
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreen : AppCompatActivity() {
+    private val splashTimeout: Long = 3000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }, splashTimeout)
+
+    }
+}
