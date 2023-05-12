b433aa8ccad9e2f6f4ca60c8e3d60876bad734e5
azizbek <abdualiev088@gmail.com>
2023-05-12 15:37:07 +0500

    Error Validation Email. Updated inputs to TaxtInputLayout.

--- app/src/main/java/com/example/house_analysis/SignInActivity.kt
+++ app/src/main/java/com/example/house_analysis/SignInActivity.kt
@@ -3,8 +3,11 @@ package com.example.house_analysis
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.addTextChangedListener
+import androidx.core.widget.doOnTextChanged
 import com.example.house_analysis.databinding.ActivityMainBinding
 import com.example.house_analysis.databinding.ActivitySignInBinding
+import com.google.android.material.textfield.TextInputLayout
 
 class SignInActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignInBinding
@@ -14,12 +17,35 @@ class SignInActivity : AppCompatActivity() {
         setContentView(binding.root)
         supportActionBar?.hide()
         forBackButton()
+
+        var editTextMail = binding.editTextMail
+        errorOccuredInEmail(editTextMail)
+
     }
 
     fun forBackButton(){
         binding.backButton.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    fun errorOccuredInEmail(editTextMail : TextInputLayout){
+        editTextMail.editText?.doOnTextChanged { inputText, _, _, _ ->
+            // Respond to input text change
+            if (inputText != null) {
+                if ("@" !in inputText){
+                    editTextMail.error = "Неверный формат почты"
+                }
+                else if ("@" in inputText){
+                    editTextMail.isErrorEnabled = false
+                }
+            }
+            else {
+                editTextMail.isErrorEnabled = false
+            }
+
+
         }
     }
 
--- app/src/main/java/com/example/house_analysis/SplashScreen.kt
+++ app/src/main/java/com/example/house_analysis/SplashScreen.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 import android.os.Handler
 
 class SplashScreen : AppCompatActivity() {
-    private val splashTimeout: Long = 3000
+    private val splashTimeout: Long = 1000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
@@ -16,6 +16,7 @@ class SplashScreen : AppCompatActivity() {
         Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+            finish()
         }, splashTimeout)
 
     }
