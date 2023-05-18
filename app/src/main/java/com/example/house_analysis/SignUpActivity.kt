6175e0a02776a7c013a79c03d2422411941bf200
azizbek <abdualiev088@gmail.com>
2023-05-19 02:56:22 +0500

    all fields cheking to be able SignUp

--- app/src/main/java/com/example/house_analysis/SignUpActivity.kt
+++ app/src/main/java/com/example/house_analysis/SignUpActivity.kt
@@ -5,15 +5,27 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log.d
+import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.content.ContextCompat.startActivity
 import androidx.core.view.isNotEmpty
+import androidx.core.widget.addTextChangedListener
 import androidx.core.widget.doOnTextChanged
 import com.example.house_analysis.databinding.ActivityMainBinding
 import com.example.house_analysis.databinding.ActivitySignUpBinding
+import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
+import com.wajahatkarim3.easyvalidation.core.view_ktx.nonEmpty
+import com.wajahatkarim3.easyvalidation.core.view_ktx.validator
 import java.util.*
 
 class SignUpActivity : AppCompatActivity() {
@@ -21,6 +33,25 @@ class SignUpActivity : AppCompatActivity() {
     private var selectedYear = 0
     private var selectedMonth = 0
     private var selectedDayOfMonth = 0
+
+//    All Inputs:
+    private lateinit var name: TextInputLayout
+    private lateinit var lastname: TextInputLayout
+    private lateinit var dateBirth: TextView
+
+    private lateinit var gender: AutoCompleteTextView
+
+    private lateinit var phone: TextInputLayout
+    private lateinit var email: TextInputLayout
+    private lateinit var password: TextInputLayout
+    private lateinit var confirmPassword: TextInputLayout
+
+    private lateinit  var iHave18: CheckBox
+    private lateinit var policy: CheckBox
+
+    private var isFieldsValid = false
+
+    private lateinit var signUpBtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +64,10 @@ class SignUpActivity : AppCompatActivity() {
         supportActionBar?.hide()
         datePicker()
         passwordsValidation()
-        checkAllInputs()
+        initFields()
+
+        validationFields(name, lastname, dateBirth, phone, email, password, confirmPassword, iHave18, policy)
+
     }
 
     fun datePicker() {
@@ -103,27 +137,76 @@ class SignUpActivity : AppCompatActivity() {
         }
 
     }
-
-    fun checkAllInputs() {
-//        d("MyLog", binding.nameText.text.toString())
-//        if (binding.nameText.text?.isEmpty() == true) d("MyLog", "${binding.nameText.text?.isNotEmpty()}")
-        if (binding.nameText.text?.length!! > 0
-//            binding.lastNameText.text?.isEmpty() == true &&
-//            binding.dateText.text.isNotEmpty() &&
-//            binding.autoCompleteText.text.isNotEmpty() &&
-//            binding.phoneText.text?.isNotEmpty() == true &&
-//            binding.emailText.text?.isNotEmpty() == true &&
-//            binding.passwordText.text?.isNotEmpty() == true &&
-//            binding.confirmPasswordText.text?.isNotEmpty() == true &&
-//            binding.iHave18checkbox.isChecked &&
-//            binding.checkboxPolicy.isChecked
-        ) {
-            val intent = Intent(applicationContext, SignInActivity::class.java).apply {
-                binding.buttonSignIn.setBackgroundColor(resources.getColor(R.color.main_color))
-                startActivity(intent)
-            }
+    fun initFields(){
+        binding.apply {
+            this@SignUpActivity.name = name
+            this@SignUpActivity.lastname = lastname
+            this@SignUpActivity.dateBirth = dateText
+            this@SignUpActivity.gender = autoCompleteText
+            this@SignUpActivity.phone = phone
+            this@SignUpActivity.email = email
+            this@SignUpActivity.password = password
+            this@SignUpActivity.confirmPassword = confirmPassword
+            this@SignUpActivity.iHave18 = iHave18checkbox
+            this@SignUpActivity.policy = checkboxPolicy
+            this@SignUpActivity.signUpBtn = buttonSignUp
         }
     }
+
+    fun validationFields(vararg fields: View) {
+        fields.forEach { field ->
+            when(field) {
+                is TextInputLayout -> {
+                    field.editText
+                        ?.validator()
+                        ?.nonEmpty()
+                        ?.addErrorCallback { updateSignUpButtonState() }
+                }
+                is CheckBox -> {
+                    field.setOnCheckedChangeListener { _, _ -> updateSignUpButtonState() }
+                }
+                is AutoCompleteTextView-> {
+                    field.addTextChangedListener(object : TextWatcher {
+                        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+                        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                            updateSignUpButtonState()
+                        }
+                        override fun afterTextChanged(s: Editable?) {}
+                    })
+                }
+                is TextView -> {
+                    field.addTextChangedListener(object : TextWatcher {
+                        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+                        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                            updateSignUpButtonState()
+                        }
+                        override fun afterTextChanged(s: Editable?) {}
+                    })
+                }
+            }
+            updateSignUpButtonState()
+        }
+    }
+    private fun updateSignUpButtonState() {
+        val isFieldsValid = areAllFieldsValid()
+
+        signUpBtn.isEnabled = isFieldsValid
+        signUpBtn.setBackgroundColor(resources.getColor(if (isFieldsValid) R.color.main_color else R.color.grey))
+        signUpBtn.setOnClickListener{ if (isFieldsValid) { startActivity(Intent(this, SignInActivity::class.java)) } else null}
+    }
+
+    private fun areAllFieldsValid(vararg textInputs: TextInputLayout): Boolean {
+        val textInputLayouts = arrayOf(name, lastname, phone, email, password, confirmPassword)
+
+        val isTextInputLayoutsValid = textInputLayouts.all { it.editText?.text?.isNotEmpty() == true }
+        val isCheckBoxesValid = iHave18.isChecked && policy.isChecked
+        val isAutoCompleteTextViewValid = gender.text?.isNotEmpty() == true
+        val isTextViewValid = dateBirth.text?.isNotEmpty() == true
+
+        return isTextInputLayoutsValid && isCheckBoxesValid && isAutoCompleteTextViewValid && isTextViewValid
+    }
+
+
 }
 
 
