415d4d1f2488debf97fe009847ab375cc30163af
azizbek <abdualiev088@gmail.com>
2023-05-23 02:54:45 +0500

    Everything is working except SingUp BTN atyle

--- app/src/main/java/com/example/house_analysis/SignUpActivity.kt
+++ app/src/main/java/com/example/house_analysis/SignUpActivity.kt
@@ -1,31 +1,14 @@
 package com.example.house_analysis
 
 import android.app.DatePickerDialog
-import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.util.Log.d
 import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
-import android.widget.Button
-import android.widget.CheckBox
-import android.widget.TextView
-import android.widget.Toast
-import androidx.core.content.ContentProviderCompat.requireContext
-import androidx.core.content.ContextCompat.startActivity
-import androidx.core.view.isNotEmpty
-import androidx.core.widget.addTextChangedListener
+import android.widget.*
 import androidx.core.widget.doOnTextChanged
-import com.example.house_analysis.databinding.ActivityMainBinding
 import com.example.house_analysis.databinding.ActivitySignUpBinding
-import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
-import com.wajahatkarim3.easyvalidation.core.view_ktx.nonEmpty
-import com.wajahatkarim3.easyvalidation.core.view_ktx.validator
 import java.util.*
 
 class SignUpActivity : AppCompatActivity() {
@@ -40,6 +23,7 @@ class SignUpActivity : AppCompatActivity() {
     private lateinit var dateBirth: TextView
 
     private lateinit var gender: AutoCompleteTextView
+    private lateinit var genderText : TextInputLayout
 
     private lateinit var phone: TextInputLayout
     private lateinit var email: TextInputLayout
@@ -50,8 +34,11 @@ class SignUpActivity : AppCompatActivity() {
     private lateinit var policy: CheckBox
 
     private var isFieldsValid = false
+    private var isMailValid = false
+    private val textInputLayouts = mutableListOf<TextInputLayout>()
 
     private lateinit var signUpBtn: Button
+    private var arePasswordsEqual: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -62,12 +49,21 @@ class SignUpActivity : AppCompatActivity() {
         val adapter = ArrayAdapter(this, R.layout.dropdown_item, items)
         binding.autoCompleteText.setAdapter(adapter)
         supportActionBar?.hide()
+        onBackBtnPressed()
+
         datePicker()
-        passwordsValidation()
         initFields()
+        passwordsValidation()
+        errorOccuredInEmail()
 
-        validationFields(name, lastname, dateBirth, phone, email, password, confirmPassword, iHave18, policy)
+        validationFields(name, lastname, gender, dateBirth, phone, email, password, confirmPassword, iHave18, policy)
 
+    }
+
+    fun onBackBtnPressed(){
+        binding.backButton.setOnClickListener {
+            finish()
+        }
     }
 
     fun datePicker() {
@@ -100,42 +96,55 @@ class SignUpActivity : AppCompatActivity() {
             datePicker.show()
             datePicker.getButton(DatePickerDialog.BUTTON_POSITIVE).text = "Choose"
         }
-        passwordsValidation()
+
 
     }
 
-    fun passwordsValidation() {
-        val LATIN_STRING: String = "a b c d e f g h i j k l m n o p q r s t u v w x y z"
-        binding.password.editText?.doOnTextChanged { text, start, before, count ->
-            if (text?.length!! > 7 && text.isNotEmpty()) {
-                binding.password.helperText = "Пароль валидный"
-            } else {
-                binding.password.isHelperTextEnabled = false
-            }
-        }
-        binding.confirmPassword.editText?.doOnTextChanged { text, start, before, count ->
-            if (text?.length!! > 7 && text.isNotEmpty()) {
-                binding.confirmPassword.helperText = "Пароль валидный"
-                d("MyLog", "${binding.confirmPassword.editText!!.text}")
-                if (binding.confirmPassword.editText!!.text.toString() == binding.password.editText?.text.toString()) {
-                    binding.confirmPassword.helperText = "Пароли совподают"
-                    binding.password.isHelperTextEnabled = false
-                    binding.confirmPassword.startIconDrawable =
-                        resources.getDrawable(R.drawable.baseline_check_circle_24)
-                    binding.password.startIconDrawable =
-                        resources.getDrawable(R.drawable.baseline_check_circle_24)
-                } else {
-                    binding.confirmPassword.helperText = resources.getString(R.string.errorOccured)
-                    binding.confirmPassword.isStartIconVisible = false
-                    binding.password.isStartIconVisible = false
-                }
-            } else {
-                binding.confirmPassword.helperText = resources.getString(R.string.errorOccured)
-                binding.confirmPassword.isStartIconVisible = false
-                binding.password.isStartIconVisible = false
-            }
-        }
+    fun passwordsValidation(){
+        var password1 = binding.password
+        var password2 = binding.confirmPassword
 
+        val LATIN_STRING: String = "a b c d e f g h i j k l m n o p q r s t u v w x y z"
+        password1.editText?.doOnTextChanged { text, start, before, count ->
+            if (text?.length!! > 7 && text.isNotEmpty()) {
+                password1.helperText = "Пароль валидный"
+            } else {
+                binding.password.error = resources.getString(R.string.errorOccured)
+            }
+            if (text.toString() == password2.editText?.text.toString()){
+                arePasswordsEqual = true
+                password1.startIconDrawable =
+                    resources.getDrawable(R.drawable.baseline_check_circle_24)
+                password2.startIconDrawable =
+                    resources.getDrawable(R.drawable.baseline_check_circle_24)
+            }
+            else {
+                password1.startIconDrawable = null
+                password2.startIconDrawable = null
+                arePasswordsEqual = false
+                password2.helperText = null
+
+            }
+
+        }
+        password2.editText?.doOnTextChanged { text, start, before, count ->
+            if (text.toString() == password1.editText?.text.toString()) {
+                arePasswordsEqual = true
+                password2.helperText = "Пароли совподают"
+                password1.helperText = null
+                password1.startIconDrawable =
+                    resources.getDrawable(R.drawable.baseline_check_circle_24)
+                password2.startIconDrawable =
+                    resources.getDrawable(R.drawable.baseline_check_circle_24)
+                password2.error = null
+            } else {
+                arePasswordsEqual = false
+                password2.error = "Пароли не совподают"
+                password1.startIconDrawable = null
+                password2.startIconDrawable = null
+            }
+
+        }
     }
     fun initFields(){
         binding.apply {
@@ -157,55 +166,79 @@ class SignUpActivity : AppCompatActivity() {
         fields.forEach { field ->
             when(field) {
                 is TextInputLayout -> {
-                    field.editText
-                        ?.validator()
-                        ?.nonEmpty()
-                        ?.addErrorCallback { updateSignUpButtonState() }
+                    textInputLayouts.add(field)
+                    field.editText?.doOnTextChanged { text, start, before, count -> updateSignUpButtonState() }
+//
                 }
                 is CheckBox -> {
-                    field.setOnCheckedChangeListener { _, _ -> updateSignUpButtonState() }
+                    field.setOnCheckedChangeListener { _, _ ->
+                        updateSignUpButtonState()
+                    }
                 }
                 is AutoCompleteTextView-> {
-                    field.addTextChangedListener(object : TextWatcher {
-                        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-                        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    field.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+                        override fun onItemSelected(
+                            parent: AdapterView<*>?,
+                            view: View?,
+                            position: Int,
+                            id: Long
+                        ) {
                             updateSignUpButtonState()
                         }
-                        override fun afterTextChanged(s: Editable?) {}
-                    })
+                        override fun onNothingSelected(parent: AdapterView<*>?) {
+//                            updateSignUpButtonState()
+                        }
+
+                    }
+                    field.doOnTextChanged { text, start, before, count ->  updateSignUpButtonState() }
                 }
                 is TextView -> {
-                    field.addTextChangedListener(object : TextWatcher {
-                        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-                        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                            updateSignUpButtonState()
-                        }
-                        override fun afterTextChanged(s: Editable?) {}
-                    })
+                    field.doOnTextChanged { text, start, before, count ->
+                        updateSignUpButtonState()
+                    }
                 }
+
             }
-            updateSignUpButtonState()
         }
+        updateSignUpButtonState()
     }
     private fun updateSignUpButtonState() {
-        val isFieldsValid = areAllFieldsValid()
+        isFieldsValid = areAllFieldsValid()
 
-        signUpBtn.isEnabled = isFieldsValid
+        binding.buttonSignUp.isEnabled = isFieldsValid
         signUpBtn.setBackgroundColor(resources.getColor(if (isFieldsValid) R.color.main_color else R.color.grey))
         signUpBtn.setOnClickListener{ if (isFieldsValid) { startActivity(Intent(this, SignInActivity::class.java)) } else null}
     }
 
-    private fun areAllFieldsValid(vararg textInputs: TextInputLayout): Boolean {
-        val textInputLayouts = arrayOf(name, lastname, phone, email, password, confirmPassword)
-
-        val isTextInputLayoutsValid = textInputLayouts.all { it.editText?.text?.isNotEmpty() == true }
+    private fun areAllFieldsValid(): Boolean {
+        val isTextInputLayoutsValid = textInputLayouts.all {
+            it.editText?.text?.isNotEmpty() == true
+        }
         val isCheckBoxesValid = iHave18.isChecked && policy.isChecked
         val isAutoCompleteTextViewValid = gender.text?.isNotEmpty() == true
         val isTextViewValid = dateBirth.text?.isNotEmpty() == true
 
-        return isTextInputLayoutsValid && isCheckBoxesValid && isAutoCompleteTextViewValid && isTextViewValid
+        return isMailValid && arePasswordsEqual && isTextInputLayoutsValid && isCheckBoxesValid && isAutoCompleteTextViewValid && isTextViewValid
     }
 
+    fun errorOccuredInEmail(){
+        email.editText?.doOnTextChanged { inputText, _, _, _ ->
+            // Respond to input text change
+            if (inputText != null && inputText.isNotEmpty()) {
+                if ("@" !in inputText){
+                    email.error = "Неверный формат почты"
+                    isMailValid = false
+                }
+                else if ("@" in inputText){
+                    email.isErrorEnabled = false
+                    isMailValid = true
+                }
+            }
+            else {
+                email.isErrorEnabled = false
+            }
+        }
+    }
 
 }
 
