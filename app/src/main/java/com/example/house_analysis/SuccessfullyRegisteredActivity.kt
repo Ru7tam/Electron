ad604ffd845f75ab8400ef3f286b8cd540beae26
Azizbek <abdualiev088@gmail.com>
2023-05-25 18:05:18 +0500

    No changes

---
+++ app/src/main/java/com/example/house_analysis/SuccessfullyRegisteredActivity.kt
@@ -0,0 +1,11 @@
+package com.example.house_analysis
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SuccessfullyRegisteredActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_successfully_registered)
+    }
+}
