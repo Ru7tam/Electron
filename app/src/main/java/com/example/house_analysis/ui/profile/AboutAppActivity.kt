196f6064d9beb3417357ccbbe5dc2957b5b76d9f
Ru7tam <105158227+Ru7tam@users.noreply.github.com>
2023-06-14 20:52:05 +0300

    Created english strings

--- app/src/main/java/com/example/house_analysis/ui/profile/AboutAppActivity.kt
+++ app/src/main/java/com/example/house_analysis/ui/profile/AboutAppActivity.kt
@@ -3,11 +3,22 @@ package com.example.house_analysis.ui.profile
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.house_analysis.R
+import com.example.house_analysis.databinding.ActivityAboutAppBinding
 
 class AboutAppActivity : AppCompatActivity() {
+    lateinit var binding:ActivityAboutAppBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_about_app)
+        binding = ActivityAboutAppBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportActionBar?.hide()
+        forBackButton()
     }
+
+    fun forBackButton(){
+        binding.backButton.setOnClickListener {
+            finish()
+        }
+    }
+
 }
--- app/src/main/java/com/example/house_analysis/ui/profile/LanguageActivity.kt
+++ app/src/main/java/com/example/house_analysis/ui/profile/LanguageActivity.kt
@@ -1,13 +1,38 @@
 package com.example.house_analysis.ui.profile
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.house_analysis.R
+import com.example.house_analysis.databinding.ActivityLanguageBinding
+import java.util.Locale
 
 class LanguageActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLanguageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_language)
+        binding = ActivityLanguageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportActionBar?.hide()
+        forBackButton()
     }
+
+    fun forBackButton(){
+        binding.backButton.setOnClickListener {
+            finish()
+        }
+    }
+
+    fun changeLanguage(){
+        binding.russianLanguageLinear.setOnClickListener {
+            val newLocale = Locale("ru")
+            Locale.setDefault(newLocale)
+            val configuration = Configuration()
+            configuration.setLocale(newLocale)
+            baseContext.resources.updateConfiguration(configuration, baseContext.resources.displayMetrics)
+            recreate()
+        }
+
+    }
+
 }
