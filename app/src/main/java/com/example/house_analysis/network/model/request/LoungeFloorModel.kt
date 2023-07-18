c1c43503a1fecdc3c7d2ea8afc0aae4cbab9b2f5
Lump <39158815+lLump@users.noreply.github.com>
2023-07-18 16:23:03 +0300

    Revert "Minimal functional"

--- app/src/main/java/com/example/house_analysis/network/model/request/LoungeFloorModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.house_analysis.network.model.request
-
-data class LoungeFloorModel(
-    val floorNumber: Int,
-    val loungeNumber: Int,
-    val subtaskIds: List<Int>
-)
--- app/src/main/java/com/example/house_analysis/ui/additional/SuccessfullyRegisteredActivity.kt
+++ app/src/main/java/com/example/house_analysis/ui/additional/SuccessfullyRegisteredActivity.kt
@@ -1,28 +1,13 @@
 package com.example.house_analysis.ui.additional
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.house_analysis.R
-import com.example.house_analysis.databinding.ActivitySuccessfullyRegisteredBinding
-import com.example.house_analysis.ui.register.SignInActivity
 
 class SuccessfullyRegisteredActivity : AppCompatActivity() {
-    private lateinit var binding: ActivitySuccessfullyRegisteredBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySuccessfullyRegisteredBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_successfully_registered)
         supportActionBar?.hide()
-
-        onOkBtnPressed()
-    }
-
-    private fun onOkBtnPressed(){
-        binding.btnSuccess.setOnClickListener {
-            val intent = Intent(this, SignInActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
     }
 }
